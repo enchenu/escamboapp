dd63017e86419a29d234780b818981c942028887
Enrique Chenú <enchenu@gmail.com>
2017-10-26 14:59:09 -0300

    Redirecionando rotas com Devise

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   end
 
   devise_for :admins, :skip => [:registrations]
-  devise_for :members
+  devise_for :members, controllers: { sessions: 'members/sessions'}
   root 'site/home#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
